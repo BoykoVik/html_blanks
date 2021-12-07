9523b3405cb492a4bbbe8e1c15d6caaa796fa107
BoykoVik <gexar47@gmail.com>
2021-12-08 02:12:52 +0300

    petshop

--- petsbar/scripts/slider.js
+++ petsbar/scripts/slider.js
@@ -1,19 +1,74 @@
+/*
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides(slideIndex += 1), 2000);
+}*/
+////----------------------------------------------------------
+
 var slideIndex = 0;
 showSlides();
 
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  //var dots = document.getElementsByClassName("dot");
+  var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
-  //for (i = 0; i < dots.length; i++) {
-  //  dots[i].className = dots[i].className.replace(" active", "");
-  //}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
   slides[slideIndex-1].style.display = "block";  
-  //dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Меняйте изображение каждые 2 секунды
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000);
 }
+
+
+function plusSlides(n) {
+    showSlides2(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides2(slideIndex = n);
+  }
+  
+  function showSlides2(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+  }
