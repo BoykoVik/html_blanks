dee3c4adfe09150a443a0ba2bdbda1d116895e0c
BoykoVik <gexar47@gmail.com>
2022-04-12 22:57:27 +0300

    84

--- hidroisolationV2/static/js/show.js
+++ hidroisolationV2/static/js/show.js
@@ -1,3 +1,4 @@
+//появление элементов
 function onEntry(entry) {
     entry.forEach(change => {
       if (change.isIntersecting) {
@@ -18,3 +19,59 @@ function onEntry(entry) {
   for (let elm of elementsright) {
     observer.observe(elm);
   }
+
+
+//переулючение вкладок медиа
+var btns = $('.btnmedia');
+var mediaitems = $('.mediacontentitem');
+
+for (i=0; i < btns.length; i++){
+  var k = btns[i];
+  $(k).attr('onClick', 'showwindow(' + i + ')');
+}
+
+function showwindow(j){
+  var a = $(".mediacontentitem")[j];
+
+  for (i=0; i < mediaitems.length; i++){
+    var k = mediaitems[i];
+    $(k).css('display', 'none');
+    btns[i].classList.remove("activemedia");
+  }
+
+  $(a).css('display', 'flex');
+  btns[j].classList.add("activemedia");
+}
+showwindow(0)
+//слайдер
+
+
+
+var slides = $('.slide');
+var nxt = $('.next');
+var prw = $('.prev');
+var nowslide = 0;
+
+function showslide(nowslide){
+  for (i=0; i < $(".slide").length; i++){
+    slides[i].classList.remove("activeslide");
+  }
+  slides[nowslide].classList.add("activeslide");
+}
+
+showslide(nowslide);
+
+$(nxt).attr('onClick', 'changeslide(1)');
+$(prw).attr('onClick', 'changeslide(-1)');
+
+function changeslide(i){
+  if ((nowslide + i) >= slides.length) {
+    nowslide = 0;
+  }
+  else  if ((nowslide + i) <= 0){
+    nowslide = slides.length-1;
+  } else {
+    nowslide += i
+  }
+  showslide(nowslide);
+}
